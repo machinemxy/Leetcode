48acc0d0761ff4558ef409c7a450099462b248f9
Ma Xueyuan <machinemxy@me.com>
2022-03-18 20:35:21 +0900

    316 & 1081

---
+++ 1081_smallest_subsequence_of_distinct_characters.swift
@@ -0,0 +1,44 @@
+// https://leetcode.com/problems/smallest-subsequence-of-distinct-characters/
+
+class Solution {
+  func smallestSubsequence(_ s: String) -> String {
+    let array = Array(s)
+
+    var lastIndexDic = Dictionary<Character, Int>()
+    for i in 0..<array.count {
+    	let char = array[i]
+    	lastIndexDic[char] = i
+    }
+
+    var addedDic = Dictionary<Character, Bool>()
+    for char in lastIndexDic.keys {
+    	addedDic[char] = false
+    }
+
+    var stack = [Character]()
+
+    for i in 0..<array.count {
+    	let char = array[i]
+
+    	// this char already in the stack
+    	if addedDic[char]! {
+    		continue
+    	}
+
+    	while stack.count > 0 && stack.last! > char {
+    		let lastChar = stack.last!
+    		if lastIndexDic[lastChar]! > i {
+    			stack.removeLast()
+    			addedDic[lastChar] = false
+    		} else {
+    			break
+    		}
+    	}
+
+    	stack.append(char)
+    	addedDic[char] = true
+    }
+
+    return String(stack)
+  }
+}
