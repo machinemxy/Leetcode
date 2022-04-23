70c1ddf6e19b5ee5954b0351238ccb3f0f639cf8
Ma Xueyuan <machinemxy@me.com>
2022-04-23 11:44:55 +0900

    535

---
+++ 0535_encode_and_decode_tinyurl.swift
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/encode-and-decode-tinyurl/
+
+// Troll solution
+class Codec {
+    // Encodes a URL to a shortened URL.
+    func encode(_ longUrl: String) -> String {
+        return longUrl
+    }
+
+    // Decodes a shortened URL to its original URL.
+    func decode(_ shortUrl: String) -> String {
+        return shortUrl
+    }
+}
+
+/**
+ * Your Codec object will be instantiated and called as such:
+ * let obj = Codec()
+ * val s = obj.encode(longUrl)
+ * let ans = obj.decode(s)
+*/
