1859977d8d9c3634e0a766f222272b93f52aef7b
Cecil Ma <machinemxy@me.com>
2022-12-12 16:30:37 +0900

    Update 0070_climbing_stairs.swift

--- 0070_climbing_stairs.swift
+++ 0070_climbing_stairs.swift
@@ -3,9 +3,7 @@
 class Solution {
 	func climbStairs(_ n: Int) -> Int {
 		guard n > 1 else { return 1 }
-		var dp = Array(repeating: 0, count: n + 1)
-		dp[0] = 1
-		dp[1] = 1
+		var dp = Array(repeating: 1, count: n + 1)
 		for i in 2...n {
 			dp[i] = dp[i - 1] + dp[i - 2]
 		}
