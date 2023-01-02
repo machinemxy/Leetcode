68e215be970f2fc61ca3b90924e6866cd943ee09
Cecil Ma <machinemxy@me.com>
2023-01-02 21:02:49 +0900

    Update 0520_detect_capital.swift

--- 0520_detect_capital.swift
+++ 0520_detect_capital.swift
@@ -1,34 +1,41 @@
-// https://leetcode.com/problems/detect-capital/
+// https://leetcode.com/problems/detect-capital/description/
 
 class Solution {
-	func detectCapitalUse(_ word: String) -> Bool {
-		let wordArray = Array(word)
-		guard wordArray.count > 1 else { return true }
-		if wordArray[0].isLowercase {
-			for i in 1..<wordArray.count {
-				if wordArray[i].isUppercase {
-					return false
-				}
-			}
-			return true
-		} else {
-			if wordArray.count == 2 {
-				return true
-			} else if wordArray[1].isLowercase {
-				for i in 2..<wordArray.count {
-					if wordArray[i].isUppercase {
-						return false
-					}
-				}
-				return true
-			} else {
-				for i in 2..<wordArray.count {
-					if wordArray[i].isLowercase {
-						return false
-					}
-				}
-				return true
-			}
-		}
-	}
+    func detectCapitalUse(_ word: String) -> Bool {
+        var pattern = Pattern.unknown
+        for c in word {
+            if c.isUppercase {
+                switch pattern {
+                    case .unknown:
+                        pattern = .firstUp
+                    case .firstUp:
+                        pattern = .firstUpSecondUp
+                    case .firstUpSecondLow:
+                        return false
+                    case .firstUpSecondUp:
+                        continue
+                    case .firstLow:
+                        return false
+                }
+            } else {
+                switch pattern {
+                    case .unknown:
+                        pattern = .firstLow
+                    case .firstUp:
+                        pattern = .firstUpSecondLow
+                    case .firstUpSecondLow:
+                        continue
+                    case .firstUpSecondUp:
+                        return false
+                    case .firstLow:
+                        continue
+                }
+            }
+        }
+        return true
+    }
+}
+
+enum Pattern {
+    case unknown, firstUp, firstUpSecondLow, firstUpSecondUp, firstLow
 }
