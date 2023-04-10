39114f4d275932d6605f632fcd9ad5814c392d8f
Cecil Ma <machinemxy@me.com>
2023-04-10 09:18:43 +0900

    Update 0020_valid_parentheses.swift

--- 0020_valid_parentheses.swift
+++ 0020_valid_parentheses.swift
@@ -1,50 +1,34 @@
 // https://leetcode.com/problems/valid-parentheses/
 
 class Solution {
-	func isValid(_ s: String) -> Bool {
-		var stack = [Character]()
+    func isValid(_ s: String) -> Bool {
+        var stack = [Character]()
 
-		for c in s {
-			switch c {
-				case "(", "{", "[":
-					stack.append(c)
-				case ")":
-					if let last = stack.last {
-						if last == "(" {
-							stack.removeLast()
-						} else {
-							return false
-						}
-					} else {
-						return false
-					}
-				case "}":
-					if let last = stack.last {
-						if last == "{" {
-							stack.removeLast()
-						} else {
-							return false
-						}
-					} else {
-						return false
-					}
-				default:
-					if let last = stack.last {
-						if last == "[" {
-							stack.removeLast()
-						} else {
-							return false
-						}
-					} else {
-						return false
-					}
-			}
-		}
+        for c in s {
+            switch c {
+                case "(", "{", "[":
+                    stack.append(c)
+                case ")":
+                    if let last = stack.last, last == "(" {
+                        stack.removeLast()
+                    } else {
+                        return false
+                    }
+                case "}":
+                    if let last = stack.last, last == "{" {
+                        stack.removeLast()
+                    } else {
+                        return false
+                    }
+                default:
+                    if let last = stack.last, last == "[" {
+                        stack.removeLast()
+                    } else {
+                        return false
+                    }
+            }
+        }
 
-		if stack.count == 0 {
-			return true
-		} else {
-			return false
-		}
-	}
+        return stack.isEmpty
+    }
 }
