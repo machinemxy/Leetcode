b8914e78a02544cb90d25da55f2abe489216f67b
Cecil Ma <machinemxy@me.com>
2023-06-30 10:56:37 +0900

    Create 2667_create_hello_world_function.js

---
+++ 2667_create_hello_world_function.js
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/create-hello-world-function/description/?envType=study-plan-v2&envId=30-days-of-javascript
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return function(...args) {
+        return "Hello World"
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
