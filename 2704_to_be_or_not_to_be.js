6b3e93ea2b39c1565b8dbca0346bb8985e219e70
Cecil Ma <machinemxy@me.com>
2023-06-30 16:23:57 +0900

    Create 2704_to_be_or_not_to_be.js

---
+++ 2704_to_be_or_not_to_be.js
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/to-be-or-not-to-be/description/
+
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    function toBe(val2) {
+        if (val === val2) return true;
+        throw new Error("Not Equal");
+    }
+    function notToBe(val2) {
+        if (val !== val2) return true;
+        throw new Error("Equal");
+    }
+    return { toBe, notToBe }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
