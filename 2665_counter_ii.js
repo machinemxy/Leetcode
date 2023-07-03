f23910ca20c85feba022b1aa976765fced3b713d
Cecil Ma <machinemxy@me.com>
2023-07-03 13:15:23 +0900

    Create 2665_counter_ii.js

---
+++ 2665_counter_ii.js
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/counter-ii/description/?envType=study-plan-v2&envId=30-days-of-javascript
+
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    var count = init
+    var increment = () => ++count
+    var decrement = () => --count
+    var reset = () => {
+        count = init
+        return count
+    }
+    return { increment, decrement, reset }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
