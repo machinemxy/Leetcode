0eb8bf2eb19dfe53b5a9e8523bd4198f1ee03af8
Cecil Ma <machinemxy@me.com>
2023-07-11 11:23:53 +0900

    Create 0863_all_nodes_distance_k_in_binary_tree.kt

---
+++ 0863_all_nodes_distance_k_in_binary_tree.kt
@@ -0,0 +1,64 @@
+// https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int = 0) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun distanceK(root: TreeNode?, target: TreeNode?, k: Int): List<Int> {
+        val paths = mutableMapOf<Int, MutableSet<Int>>()
+        val deque = ArrayDeque<TreeNode>()
+        deque.add(root!!)
+        while (deque.isNotEmpty()) {
+            val node = deque.removeFirst()
+            node.left?.let { left ->
+                paths[node.`val`]?.let {
+                    it.add(left.`val`)
+                } ?: run {
+                    paths[node.`val`] = mutableSetOf(left.`val`)
+                }
+                paths[left.`val`]?.let {
+                    it.add(node.`val`)
+                } ?: run {
+                    paths[left.`val`] = mutableSetOf(node.`val`)
+                }
+                deque.add(left)
+            }
+            node.right?.let { right ->
+                paths[node.`val`]?.let {
+                    it.add(right.`val`)
+                } ?: run {
+                    paths[node.`val`] = mutableSetOf(right.`val`)
+                }
+                paths[right.`val`]?.let {
+                    it.add(node.`val`)
+                } ?: run {
+                    paths[right.`val`] = mutableSetOf(node.`val`)
+                }
+                deque.add(right)
+            }
+        }
+        val result = ArrayDeque<Int>()
+        result.add(target!!.`val`)
+        val visited = mutableSetOf<Int>()
+        visited.add(target!!.`val`)
+        var restK = k
+        while (restK > 0) {
+            val n = result.size
+            for (i in 1..n) {
+                val node = result.removeFirst()
+                paths[node]?.forEach { 
+                    if (!visited.contains(it)) {
+                        visited.add((it))
+                        result.add(it)
+                    }
+                }
+            }
+            restK--
+        }
+        return result.toList()
+    }
+}
