fe6399051858dd2534f120a8ada9c7f841023171
Cecil Ma <machinemxy@me.com>
2024-07-05 13:23:28 +0900

    Create 2058_find_the_minimum_and_maximum_number_of_nodes_between_critical_points.kt

---
+++ 2058_find_the_minimum_and_maximum_number_of_nodes_between_critical_points.kt
@@ -0,0 +1,48 @@
+// https://leetcode.com/problems/find-the-minimum-and-maximum-number-of-nodes-between-critical-points/description/
+
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun nodesBetweenCriticalPoints(head: ListNode?): IntArray {
+        var minDistance = Int.MAX_VALUE
+        var maxDistance = Int.MIN_VALUE
+        var firstCriticalPointId = -1
+        var previousCriticalPointId = -1
+        var i = 1
+        var node = head!!.next!!
+        var prevNode = head!!
+        while (true) {
+            if (node.next == null) {
+                break
+            }
+
+            if ((node.`val` > prevNode.`val` && node.`val` > node.next!!.`val`) || (node.`val` < prevNode.`val` && node.`val` < node.next!!.`val`)) {
+                if (firstCriticalPointId == -1) {
+                    firstCriticalPointId = i
+                } else {
+                    maxDistance = i - firstCriticalPointId
+                }
+                if (previousCriticalPointId != -1) {
+                    minDistance = min(minDistance, i - previousCriticalPointId)
+                }
+                previousCriticalPointId = i
+            }
+
+            prevNode = node
+            node = node.next!!
+            i++
+        }
+        if (minDistance == Int.MAX_VALUE) {
+            return intArrayOf(-1, -1)
+        } else {
+            return intArrayOf(minDistance, maxDistance)
+        }
+    }
+}
